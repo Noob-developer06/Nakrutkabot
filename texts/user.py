d348b4e4aa5525a165067fcc623449b221357935
Noob-developer <permanshamuratov@gmail.com>
2026-02-27 10:55:00 +0500

    Add initial user messages in user.py

---
+++ texts/user.py
@@ -0,0 +1,144 @@
+msg1 = "<b>👋Assalomu aleykum botimizga hush kelibsiz. Kerakli menyuni tanlang!</b>"
+msg2 = "<b>🖇Quyidagilardan tarmoqlardan birini tanlang</b>"
+msg3 = "<b>🖇Quyidagi xizmatlardan birini tanlang</b>"
+msg4 = "<b>✅O'zingizga kerakli xizmatni tanlang</b>"
+
+
+msg6 = """<b>🛒 Buyurtma miqdorini kiriting
+🔽Min: {min}
+🔼Max: {max}</b>"""
+
+msg5 = """{service_name}
+🆔️ Xizmat ID: {service_id} ✅
+
+💶 Narx(x1000): {price}
+Chegirma: {chegirma}%
+<blockquote>
+Ushbu xizmatga {service_order_count} ta buyurtma berilgan
+
+{description}
+
+🕑Ortacha bajarish vaqti: {avg_time}
+♻️Qayta tiklash: {refill}
+❌️Bekor qilish: {cancel}
+</blockquote>
+🔽Min: {min}
+🔼Max: {max} 
+"""
+
+msg7 = """<b>📎Buyurtma uchun url manzil yuboring!</b>
+
+🌐Misol uchun: https://abc"""
+
+msg30 = """<b>⚠️ Noto'g'ri manzil kiritildi!
+Iltimos url manzilni to'g'ri kiriting!</b>"""
+
+msg8 = """<b>✅Buyurtma manzili qabul qilindi
+
+🆔Xizmat IDsi: {id}
+⚡️Xizmat nomi: {name}
+🛒Buyurtma miqdori: {quantity}
+🖇Buyurtma manzili: {link} 
+💵Buyurtma narxi: {price} so'm
+
+‼️ Malumotlar tog'ri bolsa ✅Yuborish tugmasini bosing!</b>"""
+
+msg9 = """<b>✅ Buyurtmangiz 🆔 <code>{order_id}</code> raqami bilan ro'yhatdan otkazildi!</b>"""
+
+msg10 = """<b>Sizning 🆔 {order_id} raqamli buyurtmangiz muvaffaqiyatli bajarildi:
+🆔 Buyurtma raqami: {order_id} 
+📎 Manzil: {link}  
+📥 Miqdor: {quantity}
+✅ Status: Bajarilgan</b>"""
+
+msg11 = """<b>Sizning 🆔 {order_id} raqamli buyurtmangiz bekor qilindi:
+🆔 Buyurtma raqami: {order_id} 
+📎 Manzil: {link}  
+📥 Miqdor: {quantity} 
+✅ Status: Bekor qilingan
+💵 Qaytarilgan mablag': {paid_amount} so'm</b>"""
+
+msg12 = """<b>👤 Sizning ID raqamingiz: <code> {user_id} </code> 
+
+💵 Balansingiz: {balance} so‘m
+📊 Buyurtmalaringiz: {orders_count} ta
+🗣 Referallaringiz: {ref_count} ta
+💰 Kiritgan pullaringiz: {deposit} so‘m</b>"""
+
+msg13 = """<b>
+👇 Sizning referal havolangiz:
+
+🔗 <code>{reflink}</code>
+<blockquote>
+‼️ Eslatma: Siz taklif qilgan do‘stingiz botga start bosib, bot bergan kanalga obuna bo‘lmasa sizga pul berilmaydi!
+
+⚠️ Soxta profillarni taklif qilish yoki yolg‘on reklama orqali foydalanuvchilar yig‘ish bloklanishingizga sabab bo‘lishi mumkin!
+</blockquote>
+✅ Har bir taklif qilgan do‘stingiz uchun sizga {ref_bonus} so‘m beriladi. ✅
+</b>"""
+
+msg14 = """<b>👆🏻 Yuqoridagi banner orqali siz do'stlaringizni osongina botga taklif eta olasiz
+ 
+🎁 Har bir taklif etilgan do'stingiz uchun siz {ref_bonus} so'm olasiz
+
+☑️ Shunday ekan do'stlaringizni taklif etib osongina hisobingini bepul to'ldirishingiz mumkin</b>"""
+
+msg15 = """<b>💵To'lov turini tanlang!</b>"""
+
+msg16 = """<b>⛔ Hisob karta raqami:</b>
+
+ <code>{karta}</code>
+
+👤 {FIO}
+
+📆 To‘lov qiling, tekshiruvdan so‘ng 5 min ichida [💠Avto to‘ldiriladi].
+
+⬇️ To‘lov summasi:
+➡ Min: {min_pay}
+🔄 Max: {max_pay}
+
+💠 Summani to‘g‘ri yozing, aks holda pullar tushmasligi mumkin.
+
+📨 Izohlarni o‘qing va
+✅ 'To‘lov qildim' tugmasini bosing."""
+
+msg17 = """<b>💰To'lov qilingan pul miqdorini kiriting!
+🔽Min: {min_pay}
+🔼Max: {max_pay} </b>"""
+
+msg18 = """<b>✅Qabul qilindi. Endi to'lov chekini yuboring!
+
+‼️To'lov chekini faqat rasm ko'rinishida yuboring hamda 1 ta chekni qayta-qayta yubormang!</b>"""
+
+msg19 = """<b>✅To'lov cheki qabul qilindi. To'lov tekshirilgandan so'ng hisobingizga pul o'tkaziladi!</b>"""
+
+msg20 = "<b>Murojaat matnini yuboring</b>"
+
+msg21 = """<b>‼️Buyurtma miqdorini notog’ri kiritildi!
+
+🔽Min: {min}
+🔼Max: {max}</b>"""
+
+msg22 = """<b>✅Hisobingizga {amount} so'm pul o'tkazildi!</b>"""
+
+msg23 = "⚠️ Kechirasiz, xizmatlar ro‘yxatini yuklashda xatolik yuz berdi.\nIltimos, keyinroq urinib ko‘ring."
+msg24 = "⚠️ Bo‘limlarni yuklashda muammo yuz berdi. Iltimos, qayta urinib ko‘ring."
+msg25 = "⚠️ Xizmatlarni yuklashda muammo yuz berdi. Iltimos, keyinroq urinib ko‘ring."
+msg26 = "⚠️ Xizmat tafsilotlarini ko‘rsatishda xatolik yuz berdi.\nIltimos, keyinroq urinib ko‘ring."
+msg27 = "❌ Bunday xizmat topilmadi yoki vaqtincha mavjud emas."
+msg28 = "⚠️ Buyurtma berishda xatolik yuz berdi. Iltimos, keyinroq urinib ko‘ring."
+
+msg29 = """<b>📋 Buyurtma ma'lumotlari:
+
+🆔 Buyurtma ID raqami: {order_id} 
+🆔️ Xizmat ID raqami: {id}
+⚡️Xizmat nomi: {service_name}
+🖇Manzili: {link}
+🛒Miqdori: {quantity}
+💵Narxi: {price} so'm
+⏱Vaqti: {created_at}
+✅Holati: {status}
+━ ━ ━ ━ ━ ━ ━ ━ ━</b>"""
+
+
+msg31 = """<b>❌ Kechirasiz, botimizdan foydalanish uchun ushbu kanallarga obuna bo'lishingiz kerak.</b>"""
