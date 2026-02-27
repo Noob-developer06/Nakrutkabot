bafa2b4141cf1631757e801ba05d4af8ea1b7341
Noob-developer <permanshamuratov@gmail.com>
2026-02-27 10:55:20 +0500

    Add payment notification texts in admin.py

---
+++ texts/admin.py
@@ -0,0 +1,28 @@
+
+text1 = """🧾 <b>Yangi to‘lov chek yuborildi</b>
+
+👤 <b>Foydalanuvchi:</b> <code>{user_id}</code>
+💰 <b>Summa:</b> {amount} so‘m
+💳 <b>To‘lov turi:</b> {pay_method}
+
+⏳ <i>Tasdiqlash kutilmoqda...</i>"""
+
+text2 = """🧾 <b>Yangi to‘lov chek yuborildi</b>
+
+👤 <b>Foydalanuvchi:</b> <code>{user_id}</code>
+💰 <b>Summa:</b> {amount} so‘m
+
+✅ <b>To‘lov tasdiqlandi!</b>"""
+
+
+text3 = """🧾 <b>Yangi to‘lov chek yuborildi</b>
+
+👤 <b>Foydalanuvchi:</b> <code>{user_id}</code>
+💰 <b>Summa:</b> {amount} so‘m
+
+<b>❌To'lov bekor qilindi!</b>"""
+
+text4 = """<b>❌ {amount} so‘m to‘lovingiz bekor qilindi.\nIltimos, qayta to‘lov yuboring va xizmatdan foydalaning 😊</b>"""
+text5 = """<b>✅ {amount} so‘m to‘lovingiz tasdiqlandi.\nXizmatdan foydalanishingiz mumkin 😊</b>"""
+
+text6 = """<b>⚙️ {domain} - {balance}  {currency} \n\n🔗 Havola: {url} \n🔑 Kalit:</b> <code>{key}</code>"""
