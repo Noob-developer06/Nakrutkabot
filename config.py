fcd5faef31b7a500a907332a1733c6558f579012
Noob-developer <permanshamuratov@gmail.com>
2026-02-27 10:44:11 +0500

    Initialize config.py with bot settings
    Added configuration settings for bot, including payment methods and currency rates.

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+
+# .env faylni yuklaymiz
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+DB_PATH = "database/data.db"
+
+ADMIN = 8483511103
+
+ref_bonus = 100
+
+karta = 8000123456789012
+FIO = "JON DOE"
+
+min_pay = 1000
+max_pay = 1000000
+
+update_status_time = 300
+
+payments_channel_id = -1003766126502
+support_channel_id = -1003406665330
+error_channel_id = -1003760597369
+baza_channel_id = -1003760597369
+
+channel_id = -1003757492292
+channel_username = "FastSmmProKanal"
+
+
+foiz = 1.5
+
+CURRENCY_RATE = {
+        "RUB": 160,
+        "USD": 12500,
+        "UZS": 1
+}
+
+
+pay_methods = {
+    "click": "⚫ Click [ AVTO ]",
+    "payme": "🔵 Payme [ AVTO ]",
+    "paynet": "🟢 Paynet",
+    "xazna": "🟡 Xazna",
+    "uzum": "🟣 Uzum ",
+    "apelsin": "🟠 Apelsin ",
+    "tbc": "🔴 TBC ",
+    "humo": "🟤 Humo ",
+    "click_cashback": "⚫ Click [ CASHBACK ]",
+    "hamkorbank": "🟦 Hamkorbank "
+}
